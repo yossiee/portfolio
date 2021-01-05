33494b94d601190edcfb6608a6bd3c3391552c06
yossiee <38056766+yossiee@users.noreply.github.com>
2021-01-05 23:59:28 +0900

    Rollback: quote single to double

--- src/components/image.js
+++ src/components/image.js
@@ -1,6 +1,6 @@
-import React from 'react'
-import { StaticQuery, graphql } from 'gatsby'
-import Img from 'gatsby-image'
+import React from "react"
+import { StaticQuery, graphql } from "gatsby"
+import Img from "gatsby-image"
 
 export default (props) => (
   <StaticQuery

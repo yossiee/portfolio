0a51c6a35b389b0106d09d720200fa9e748227bc
yossiee <38056766+yossiee@users.noreply.github.com>
2021-01-05 17:55:56 +0900

    Delete: `h1` tag from haeder component

--- src/components/header.js
+++ src/components/header.js
@@ -16,17 +16,15 @@ const Header = ({ siteTitle }) => (
         padding: `1.45rem 1.0875rem`,
       }}
     >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+      <Link
+        to="/"
+        style={{
+          color: `white`,
+          textDecoration: `none`,
+        }}
+      >
+        {siteTitle}
+      </Link>
     </div>
   </header>
 )

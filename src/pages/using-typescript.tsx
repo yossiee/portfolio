df5b001671b44a421a95e2dfb2410d6245e842af
yossiee <38056766+yossiee@users.noreply.github.com>
2021-01-05 21:44:34 +0900

    Check: japanese font

--- src/pages/using-typescript.tsx
+++ src/pages/using-typescript.tsx
@@ -14,7 +14,7 @@ type DataProps = {
 const UsingTypescript: React.FC<PageProps<DataProps>> = ({ data, path }) => (
   <Layout>
     <SEO title="Using TypeScript" />
-    <h1>Gatsby supports TypeScript by default!</h1>
+    <h1>Gatsby はデフォルトで TypeScript をサポートしています！</h1>
     <p>
       This means that you can create and write <em>.ts/.tsx</em> files for your
       pages, components etc. Please note that the <em>gatsby-*.js</em> files

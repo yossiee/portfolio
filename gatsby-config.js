7d5401aae93692f8b53dad1dc7683a10868be240
yossiee <38056766+yossiee@users.noreply.github.com>
2021-01-05 16:33:05 +0900

    Fix: theme color

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,10 +21,10 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#3c4c6c`,
+        theme_color: `#3c4c6c`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/favicon.svg`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality

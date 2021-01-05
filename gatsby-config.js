20c8ac141cb178053bf4c93ee78e30027e915ee5
yossiee <38056766+yossiee@users.noreply.github.com>
2021-01-05 21:44:01 +0900

    Introduce: `typography`

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,11 +24,14 @@ module.exports = {
         background_color: `#3c4c6c`,
         theme_color: `#3c4c6c`,
         display: `minimal-ui`,
-        icon: `src/images/favicon.ico`, // This path is relative to the root of the site.
+        icon: `src/images/favicon.png`,
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
+      },
+},
   ],
 }
